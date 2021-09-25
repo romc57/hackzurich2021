6c6bc899b483b2a0bcbb65e90fa429f1d79b8cfc
romcohen <romcohen@cs.huji.ac.il>
2021-09-25 20:41:39 +0300

    ,

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ def hello_world():
 def get_genre_weights():
     user = fitrockr_api.get_user_info('new user RC')  # Query user named Rom
     user_id = user[0]['id']
-    return 'Mira stress: {}'.format(fitrockr_api.get_user_stress_records(user_id, fitrockr_api.time_to_sec('10', '12', '00')))
+    stress = fitrockr_api.get_user_stress_level(user_id, fitrockr_api.time_to_sec('12', '12', '00'))
+    return 'Mira stress: {}'.format(stress)
 
 
 if __name__ == '__main__':
